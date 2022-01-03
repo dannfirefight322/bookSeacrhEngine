a835b5d83a50f49fb716f0049f2c5f0af332a472
unknown <amery.bur410@gmail.com>
2022-01-03 01:04:37 -0600

    updating server.

--- server/server.js
+++ server/server.js
@@ -9,17 +9,12 @@ const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
-
-//apply apollo server with express app
-server.applyMiddleware({ app });
-
-//middleware parsing
+server.applyMiddleware({app});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -28,14 +23,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-
-//get all
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
